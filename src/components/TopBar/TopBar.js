64150f1295f4677c01fc51da60269ae40d21f40d
LucianoVeiga <52001102+LucianoVeiga@users.noreply.github.com>
2020-11-17 14:41:51 -0300

    Update TopBar.js

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -20,10 +20,10 @@ class TopBar extends React.Component {
     render() {
         return (
             <header>
-                <img src="Images/Icono.png" alt="logo" id="icon"></img>
+                <img src="https://firebasestorage.googleapis.com/v0/b/dibujando-9b19b.appspot.com/o/icons%2FIcono.png?alt=media&token=d277cb99-9842-443f-aef3-2ff87cc1aaa0" alt="logo" id="icon"></img>
                 <a href="/upload" id="subir"><button className="botones">Subir Dibujo</button></a>
                 <div id="derecha">
-                    <a style={{ float: "right" }} id="perfil" href="/profile"><img src="Images/Profile.png" alt="perfil" id="logo"></img></a>
+                    <a style={{ float: "right" }} id="perfil" href="/profile"><img src="https://firebasestorage.googleapis.com/v0/b/dibujando-9b19b.appspot.com/o/icons%2FProfile.png?alt=media&token=49097c46-69f8-4759-9e53-9bdb35b44add" alt="perfil" id="logo"></img></a>
                     <button onClick={() => app.auth().signOut()} style={{ float: "right" }} className="botones" id="logout">Logout</button>
                     <label className="nbre">{this.usuario}</label>
                 </div>
